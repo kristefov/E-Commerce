ca80286cd07f75ad3c1ae8bfb10f012e76fdfd7a
kristiyan <kristiyan.tefov@icloud.com>
2023-04-21 18:56:01 +0100

    models created

---
+++ Develop/routes/api/category-routes.js
@@ -0,0 +1,50 @@
+const router = require("express").Router();
+const { Category, Product } = require("../../models");
+
+// The `/api/categories` endpoint
+
+router.get("/", async (req, res) => {
+  // find all categories
+  // be sure to include its associated Products
+  try {
+    const categoryData = await Category.findAll({
+      include: Product,
+    });
+    res.status(200).json(categoryData);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json(err);
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  // find one category by its `id` value
+  // be sure to include its associated Products
+  try {
+    const categoryData = await Category.findByPk(req.params.id, {
+      include: Product,
+    });
+    if (!categoryData) {
+      res.status(404).send("No Category with that ID");
+    } else {
+      res.status(200).json(categoryData);
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).json(err);
+  }
+});
+
+router.post("/", (req, res) => {
+  // create a new category
+});
+
+router.put("/:id", (req, res) => {
+  // update a category by its `id` value
+});
+
+router.delete("/:id", (req, res) => {
+  // delete a category by its `id` value
+});
+
+module.exports = router;
